7dd9f97aafdaa005ce933c7a9754e6de97693bf8
cneel360 <128312526+cneel360@users.noreply.github.com>
2025-06-14 11:36:58 -0400

    AimSensitivity InDev

--- Assets/player/scripts/playershootingsystem.cs
+++ Assets/player/scripts/playershootingsystem.cs
@@ -1,17 +1,25 @@
 using UnityEngine;
 using Cinemachine;
 using StarterAssets;
+using UnityEngine.UIElements;
 
 public class PlayerShootingSystem : MonoBehaviour
 {
     [SerializeField] private CinemachineVirtualCamera aimcam;
-    [SerializeField] private  StarterAssetsInputs inputmanager;
+    [SerializeField] private StarterAssetsInputs inputmanager;
+    private ThirdPersonController startercontroller;
+    [SerializeField] private float aimSensitivity;
+    [SerializeField] private float normalSensitivity;
 
     void Awake()
     {
         if (inputmanager == null)
         {
             inputmanager = GetComponent<StarterAssetsInputs>();
+        }
+        if (startercontroller == null)
+        {
+            startercontroller = GetComponent<ThirdPersonController>();
         }
 
         if (aimcam == null)
@@ -24,15 +32,17 @@ public class PlayerShootingSystem : MonoBehaviour
     {
         if (inputmanager != null)
         {
-         //   Debug.Log("Aim input state: " + inputmanager.aim);
+            //   Debug.Log("Aim input state: " + inputmanager.aim);
 
             if (inputmanager.aim)
             {
                 aimcam.gameObject.SetActive(true);
+                startercontroller.SetCameraSensitivity(aimSensitivity);
             }
             else
             {
                 aimcam.gameObject.SetActive(false);
+                startercontroller.SetCameraSensitivity(normalSensitivity);
             }
         }
         else
