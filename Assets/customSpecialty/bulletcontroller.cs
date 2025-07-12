e47fece7ffd95eaad5d5bcb513315590b37ad5ad
cneel360 <128312526+cneel360@users.noreply.github.com>
2025-07-12 12:46:30 -0400

    eod 7 12 25

--- Assets/customSpecialty/bulletcontroller.cs
+++ Assets/customSpecialty/bulletcontroller.cs
@@ -4,6 +4,8 @@ public class bulletcontroller : MonoBehaviour
 {
     [SerializeField] float speed;
     private Rigidbody rb;
+ [SerializeField]   private float excepttime = 20f;
+   [SerializeField] private  bool istimerrunning;
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
@@ -11,18 +13,33 @@ public class bulletcontroller : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-       // speed = 10f;
+        istimerrunning = true;
+
+        // speed = 10f;
         rb.linearVelocity = transform.forward * speed;
+        
         
     }
 
     void OnTriggerEnter(Collider other)
     {
+        speed = 0f;
+        Debug.Log("Collided with: " + other);
         Destroy(gameObject);
     }
     // Update is called once per frame
     void Update()
     {
-        
+        if (istimerrunning)
+        {
+            excepttime -= Time.deltaTime;
+
+        }
+
+        if (excepttime <= 0)
+        {
+            Debug.Log("Despawned");
+            Destroy(gameObject);
+        }
     }
 }
