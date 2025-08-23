07118e36b70b57dba0e4558f777e161a7c51d180
cneel360 <128312526+cneel360@users.noreply.github.com>
2025-08-23 10:56:42 -0400

    healthSys

--- Assets/player/scripts/playerhealth.cs
+++ Assets/player/scripts/playerhealth.cs
@@ -1,16 +1,39 @@
 using UnityEngine;
 
 public class playerhealth : MonoBehaviour
+
 {
+    public float healthbuffer;
+    public float maxhealth;
+    public float protection;
+    public float health;
+    public float finaldamage;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        healthbuffer = maxhealth -= 1;
     }
+    public void Damage(float damagenum)
+    {
+        finaldamage = damagenum - protection;
+        health -= finaldamage;
+    }
+    public void Heal()
+    {
+
+        if (health < healthbuffer)
+        {
+            health += 1;
+        }
+    }
+        
+    
 }
