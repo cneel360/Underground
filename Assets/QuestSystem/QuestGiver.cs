26f468dd710ef1bd3fe0ddf54aaadc42092bb50f
cneel360 <128312526+cneel360@users.noreply.github.com>
2026-02-01 16:37:34 -0500

    436 2-1-2026

--- Assets/QuestSystem/QuestGiver.cs
+++ Assets/QuestSystem/QuestGiver.cs
@@ -8,7 +8,7 @@ public class QuestGiver : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        quest = manager.GetQuest(0);
+        quest = manager.GetQuest(qid);
     }
  public void GiveQuest()
     {
