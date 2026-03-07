a58c52635b0b3c81e74aba585bdc554a5433a82f
cneel360 <128312526+cneel360@users.noreply.github.com>
2026-03-06 20:32:06 -0500

    eod 3-6-2026

--- Assets/ui/Scripts/UILayerController.cs
+++ Assets/ui/Scripts/UILayerController.cs
@@ -7,12 +7,36 @@ public class UILayerController : MonoBehaviour
 
     public bool runonindex;
     public int layerindex;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if (OnTop)
+        {
+            GoOnTop();
+        }
+        if (OnBottom)
+        {
+            GoOnBottom();
+        }
+        if (runonindex)
+        {
+            GobyIndex();
+        }
     }
-
+    void GoOnTop()
+    {
+        transform.SetAsLastSibling();
+    }
+    void GoOnBottom()
+    {
+        transform.SetAsFirstSibling();
+    }
+    void GobyIndex()
+    {
+        transform.SetSiblingIndex(layerindex);
+    }
     // Update is called once per frame
     void Update()
     {
