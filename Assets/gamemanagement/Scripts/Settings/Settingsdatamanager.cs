dd1330cf1100cc214f2bacc2e9b89a6c872e7d2a
cneel360 <128312526+cneel360@users.noreply.github.com>
2026-04-17 20:29:57 -0400

    EOD 4-17-2026

--- Assets/gamemanagement/Scripts/Settings/Settingsdatamanager.cs
+++ Assets/gamemanagement/Scripts/Settings/Settingsdatamanager.cs
@@ -1,11 +1,54 @@
+using JetBrains.Annotations;
 using UnityEngine;
-
+using System;
+using UnityEngine.Rendering;
 public class Settingsdatamanager : MonoBehaviour
 {
+    public string basepath;
+    public string filepath;
+    public settingsdata data;
+    public string jsondata;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+      basepath = Application.persistentDataPath;
+      filepath = basepath + "/basicgamesettings.json";
+      if (System.IO.File.Exists(filepath))
+        {
+            Load();
+        }
+        else
+        {
+            Generatebasedata();
+            Save();
+        }
+       
+    }
     void Start()
     {
         
+    }
+       void Load()
+    {
+       
+        {
+            jsondata = System.IO.File.ReadAllText(filepath);
+            data = JsonUtility.FromJson<settingsdata>(jsondata); 
+            Debug.Log("Data Loaded");
+        }
+       
+    }
+    void Generatebasedata()
+    {
+        data.volume = 1;
+    }
+     public void Save()
+    {
+        jsondata = JsonUtility.ToJson(data);
+        System.IO.File.WriteAllText(filepath, jsondata);
+        Debug.Log("game saved");
+
+
     }
 
     // Update is called once per frame
@@ -13,4 +56,9 @@ public class Settingsdatamanager : MonoBehaviour
     {
         
     }
+}
+[System.Serializable]
+public  class settingsdata
+{
+    public float volume;
 }
--- Assets/gamemanagement/Scripts/Settings/VolumeController.cs
+++ Assets/gamemanagement/Scripts/Settings/VolumeController.cs
@@ -2,10 +2,21 @@ using UnityEngine;
 using UnityEngine.UI;
 public class VolumeController : MonoBehaviour
 {
+   public  Settingsdatamanager datamanager;
+     public Slider volumeSlider;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        ChangeVolume(datamanager.data.volume);
+        volumeSlider.value = datamanager.data.volume;
+    }
 public void ChangeVolume(float value)
     {
         Debug.Log("The new value is: " + value);
          AudioListener.volume = value;
+    }
+    public void SaveValue(float value)
+    {
+        datamanager.data.volume = value;
     }
 }
